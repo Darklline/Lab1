2b2fc196f1d92579775a8c61723cc1df558b5ff1
Darklline <ivaxy12@gmail.com>
2021-03-07 14:24:45 +0100

    impr

--- index.ts
+++ index.ts
@@ -10,18 +10,53 @@ class StatisticsApp
     minInput: HTMLInputElement;
     maxInput: HTMLInputElement;
 
+    constructor()
+    {
+        this.startApp();
+    }
+
     startApp()
     {
         this.getInputs();
         this.inputsObserver();
     }
-    inputsObserver() {
-        throw new Error("Method not implemented.");
+
+    inputsObserver() 
+    {
+        this.data1Input.addEventListener("input", () => this.getStatistics());
+        this.data1Input.addEventListener("input", () => this.getStatistics());
+        this.data1Input.addEventListener("input", () => this.getStatistics());
+        this.data1Input.addEventListener("input", () => this.getStatistics());
     }
-    getInputs() {
-        throw new Error("Method not implemented.");
+
+    getInputs() 
+    {
+        this.data1Input = document.querySelector('#data1');
+        this.data2Input = document.querySelector('#data2');
+        this.data3Input = document.querySelector('#data3');
+        this.data4Input = document.querySelector('#data4');
+
+        this.sum1Input = document.querySelector('#sum');
+        this.avgInput = document.querySelector('#avg');
+        this.minInput = document.querySelector('#min');
+        this.maxInput = document.querySelector('#max');
     }
-    getStatistics(): any {
-        throw new Error("Method not implemented.");
+
+    getValue(element : HTMLInputElement) : number {
+        const {value} = element;
+        return Number.isInteger(value) ? parseInt(value) : 0;
+    }
+
+    getStatistics()
+    {
+        const data1 = this.getValue(this.data1Input);
+        const data2 = this.getValue(this.data2Input);
+        const data3 = this.getValue(this.data3Input);
+        const data4 = this.getValue(this.data4Input);
+
+        const sum = data1 + data2 + data3 + data4;
+        const avg = sum / 4;
+        const min = Math.min(data1, data2, data3, data4);
+        const max = Math.max(data1, data2, data3, data4);
     }
 }
