ff16928a92081c327fbfe553eee22e373662e3f2
Darklline <ivaxy12@gmail.com>
2021-06-24 19:02:14 +0200

    impr

--- Lab4/index.ts
+++
@@ -1,9 +0,0 @@
-import { INote } from './types';
-import { AppStorage } from './AppStorage';
-import { AppNotes } from './AppNotes';
-import './main.scss';
-//import { FireStorage } from './FireStorage';
-
-const noteStorage = new AppStorage<INote>();
-//const fireStorage = new FireStorage<INote>();
-const appNotes = new AppNotes(noteStorage);
--- Lab4/typeGuards.ts
+++
@@ -1,3 +0,0 @@
-import { INote } from './types';
-
-export const isNoteType = (item: any): item is INote => 'Title' in item && 'Pinned' in item;
